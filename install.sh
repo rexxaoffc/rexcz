54f71fbe5e8385d2457cb572d43578c73977f749
sevsbotz <sevsstore0@gmail.com>
2025-01-04 07:18:10 +0700

    install.sh
    Signed-off-by: sevsbotz <sevsstore0@gmail.com>

--- install.sh
+++ install.sh
@@ -328,11 +328,11 @@ cd /var/www/pterodactyl || { echo "Direktori tidak ditemukan"; exit 1; }
 # Membuat lokasi baru
 php artisan p:user:make <<EOF
 yes
-lexcz@lexcz.xyz
-lexcz
-lexcz
-lexcz
-lexcz
+sevs@sevsbotz.xyz
+sevsbotz
+sevsbotz
+sevsbotz
+sevsbotz
 EOF
   echo -e "                                                       "
   echo -e "${GREEN}[+] =============================================== [+]${NC}"
@@ -352,10 +352,10 @@ while true; do
   echo -e "${RED}  ,ggggg        gggggggg.                                            ${NC}"
   echo -e "${RED} ,ggg'               'ggg.                                           ${NC}"
   echo -e "${RED}',gg       ,ggg.      'ggg:                                          ${NC}"
-  echo -e "${RED}'ggg      ,gg'''  .    ggg       Auto Installer LeXcZ   ${NC}"
+  echo -e "${RED}'ggg      ,gg'''  .    ggg       Auto Installer SEVSBOTZ   ${NC}"
   echo -e "${RED}gggg      gg     ,     ggg      -----------------------------------  ${NC}"
-  echo -e "${RED}ggg:     gg.     -   ,ggg       • Telegram : @LeXcZ_Alok         ${NC}"
-  echo -e "${RED} ggg.    '.'''ggggggp           • Support By LeXcZ  ${NC}"
+  echo -e "${RED}ggg:     gg.     -   ,ggg       • Telegram : @sevskmtl        ${NC}"
+  echo -e "${RED} ggg.    '.'''ggggggp           • Support By SEVSBOTZ  ${NC}"
   echo -e "${RED}  'ggg    '-.__                                                      ${NC}"
   echo -e "${RED}    ggg                                                              ${NC}"
   echo -e "${RED}      ggg                                                            ${NC}"
